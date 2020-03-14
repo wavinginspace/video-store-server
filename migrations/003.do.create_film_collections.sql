10c825a4341b71ff682af5c8b45aeed2b41e3763
wavinginspace <paulbaisley@gmail.com>
2020-03-14 16:16:57 -0400

    delete films working

--- migrations/003.do.create_film_collections.sql
+++ migrations/003.do.create_film_collections.sql
@@ -1,5 +1,5 @@
 CREATE TABLE film_collections (
-  film_id INTEGER REFERENCES films(id),
+  film_id INTEGER REFERENCES films(id) ON DELETE CASCADE NOT NULL,
   collection_id INTEGER REFERENCES collections(id),
   PRIMARY KEY (film_id, collection_id)
 );
