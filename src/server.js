5232bab319822316786464025594e988d76bfd40
wavinginspace <paulbaisley@gmail.com>
2020-03-15 16:09:46 -0400

    fixed date_added formatting

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const app = require('./app');
 const knex = require('knex');
 const { PORT, DATABASE_URL } = require('./config');
+const moment = require ('moment');
+const { types } = require('pg');
+const { builtins } = require('pg-types');
+
+const parseFn = (val) => {
+  return val === null ? null : moment(val).format('MM-DD-YYYY');
+};
+
+types.setTypeParser(builtins.DATE, parseFn);
 
 const db = knex({
   client: 'pg',
